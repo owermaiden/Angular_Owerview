b602b097cc8ed8caa3b23ffa5789f83a47849950
owermaiden <ower.maiden@cybertekschool.com>
2021-12-08 17:22:18 +0300

    angular-ecommerce

--- ecommerce-project/02-backend-files/spring-boot-ecommerce/src/main/java/com/ower/ecommerce/config/MyDataRestConfig.java
+++ ecommerce-project/02-backend-files/spring-boot-ecommerce/src/main/java/com/ower/ecommerce/config/MyDataRestConfig.java
@@ -1,2 +1,31 @@
-package com.ower.ecommerce.config;public class MyDataRestConfig {
+package com.ower.ecommerce.config;
+
+import com.ower.ecommerce.entity.Product;
+import com.ower.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        // disable HTTP methods for Product: PUT, POST, DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // disable HTTP methods for ProductCategory: PUT, POST, DELETE
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
 }
--- ecommerce-project/02-backend-files/spring-boot-ecommerce/src/main/java/com/ower/ecommerce/dao/ProductRepository.java
+++ ecommerce-project/02-backend-files/spring-boot-ecommerce/src/main/java/com/ower/ecommerce/dao/ProductRepository.java
@@ -1,2 +1,11 @@
-package com.ower.ecommerce.dao;public interface ProductRepository {
+package com.ower.ecommerce.dao;
+
+import com.ower.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Repository
+@CrossOrigin
+public interface ProductRepository  extends JpaRepository<Product, Long> {
 }
