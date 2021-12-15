890800bcda909f3d551bdbdd7ecfbcd609b49acd
owermaiden <ower.maiden@cybertekschool.com>
2021-12-15 21:26:16 +0300

    router-outlet

--- ecommerce-project/02-backend-files/spring-boot-ecommerce/src/main/java/com/ower/ecommerce/dao/ProductRepository.java
+++ ecommerce-project/02-backend-files/spring-boot-ecommerce/src/main/java/com/ower/ecommerce/dao/ProductRepository.java
@@ -1,11 +1,18 @@
 package com.ower.ecommerce.dao;
 
 import com.ower.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 @Repository
 @CrossOrigin
 public interface ProductRepository  extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
