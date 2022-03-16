b7fa1858fe462550ba27adeb091f8eb307c82412
owermaiden <ower.maiden@cybertekschool.com>
2022-03-17 01:25:09 +0300

    some changes

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { pipe, switchMap } from 'rxjs';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -20,30 +19,29 @@ export class ProductListComponent implements OnInit {
 
   ngOnInit() { 
     this.route.paramMap.subscribe((params: ParamMap) => {
-      this.currentCategoryId = parseInt(params.get('id')!);
-      this.searchWord = params.get('keyword')!;
-      if (this.currentCategoryId){
-        this.listProducts(this.currentCategoryId);
-      } else if (this.searchWord){
-        this.listSearchProducts(this.searchWord);
-      }
+      this.listProductsBy(params);
     });
   }
 
-  listSearchProducts(searchWord: string) {
-    this.productService.getSearchProductList(searchWord).subscribe(
-      data => {
-        this.products = data;
-      }
-    ) 
+  listProductsBy(params: ParamMap){
+
+    this.currentCategoryId = parseInt(params.get('id')!);
+    this.searchWord = params.get('keyword')!;
+
+    if (this.currentCategoryId){
+      this.productService.getProductList(this.currentCategoryId).subscribe(
+        data => {
+          this.products = data;
+        }
+      ) 
+    } else if (this.searchWord){
+      this.productService.getSearchProductList(this.searchWord).subscribe(
+        data => {
+          this.products = data;
+        }
+      ) 
+    }
   }
 
-  listProducts(id: number) {    
-    this.productService.getProductList(id).subscribe(
-      data => {
-        this.products = data;
-      }
-    )        
-  }
 
 }
