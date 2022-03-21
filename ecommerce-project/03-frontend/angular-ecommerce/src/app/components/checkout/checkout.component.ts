2ea1926d2f6025c73b1c5e3aabc0a273db0fa176
owermaiden <ower.maiden@cybertekschool.com>
2022-03-21 21:37:28 +0300

    s

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Country } from 'src/app/common/country';
+import { State } from 'src/app/common/state';
+import { ShoppingService } from 'src/app/services/shopping.service';
 
 @Component({
   selector: 'app-checkout',
@@ -13,7 +16,16 @@ export class CheckoutComponent implements OnInit {
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
-  constructor(private formBuilder: FormBuilder) { }
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
+
+  countries: Country[] = [];
+
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
+
+  constructor(private formBuilder: FormBuilder,
+              private shoppingService: ShoppingService) { }
 
   ngOnInit(): void {
 
@@ -46,7 +58,39 @@ export class CheckoutComponent implements OnInit {
         expirationYear: ['']
       })
     });
-  }
+
+    // populate credit card months
+
+  const startMonth: number = new Date().getMonth() + 1;
+  console.log("startMonth: " + startMonth);
+
+  this.shoppingService.getCreditCardMonths(startMonth).subscribe(
+    data => {
+      console.log("Retrieved credit card months: " + JSON.stringify(data));
+      this.creditCardMonths = data;
+    }
+  );
+
+  // populate credit card years
+
+  this.shoppingService.getCreditCardYears().subscribe(
+    data => {
+      console.log("Retrieved credit card years: " + JSON.stringify(data));
+      this.creditCardYears = data;
+    }
+  );
+
+  // populate countries
+
+  this.shoppingService.getCountries().subscribe(
+    data => {
+      console.log("Retrieved countries: " + JSON.stringify(data));
+      this.countries = data;
+    }
+  );
+
+  } // ngOninit finishes here...
+  
 
   copyShippingAddressToBillingAddress(event: { target: HTMLInputElement; }) {
 
@@ -61,9 +105,68 @@ export class CheckoutComponent implements OnInit {
   }
 
   onSubmit() {
+
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get('customer')!.value);
-    console.log("The email address is " + this.checkoutFormGroup.get('customer')!.value.email);
+    console.log("The email address is " + this.checkoutFormGroup.get('customer')!.value.email);  
+    console.log("The shipping address country is " + this.checkoutFormGroup.get('shippingAddress')!.value.country.name);
+    console.log("The shipping address state is " + this.checkoutFormGroup.get('shippingAddress')!.value.state.name);
   }
+
+
+  handleMonthsAndYears() {
+
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup!.value.expirationYear);
+
+    // if the current year equals the selected year, then start with the current month
+
+    let startMonth: number;
+
+    if (currentYear === selectedYear) {
+      startMonth = new Date().getMonth() + 1;
+    }
+    else {
+      startMonth = 1;
+    }
+
+    this.shoppingService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    );
+  }
+
+  getStates(formGroupName: string) {
+
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+
+    const countryCode = formGroup!.value.country.code;
+    const countryName = formGroup!.value.country.name;
+
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
+
+    this.shoppingService.getStates(countryCode).subscribe(
+      data => {
+
+        if (formGroupName === 'shippingAddress') {
+          this.shippingAddressStates = data; 
+        }
+        else {
+          this.billingAddressStates = data;
+        }
+
+        // select first item by default
+        formGroup!.get('state')!.setValue(data[0]);
+      }
+    );
+  }
+
+
+
 
 }
